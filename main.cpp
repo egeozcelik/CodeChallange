61d46c90140ac9ca0db83ebf65181ccb022c7c3a
egeozcelik <e4eozcelik@gmail.com>
2021-02-11 14:04:26 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> minimalHeaviestSetA(vector<int> arr)
+{
+    //Step 1. Calculating the total amount of elements of arr given in main func.
+    int totalAmount=0;
+    int ArraySize= arr.size();
+    for(int i=0; i<ArraySize; i++)
+    {
+        totalAmount += arr[i];
+    }
+    // step 2. If the sum of the values ​​of A is greater than that of A and A should be minimal,
+    // the sum of the values of A must be equal to the minimum array that is greater than half the sum of the values.
+    // for example totalAmount equals to 51,total amount of A equals to 26 or higher.But we want that A should be minimum so total amount of A
+    // must be close to 26.
+    int valueToReach=0;
+    if(totalAmount%2 == 0)
+    {
+        valueToReach = (totalAmount / 2) + 1;
+    }
+    else
+        valueToReach = (totalAmount+1)/ 2;
+
+    //step 3. Lets sort the arr from bigger to smaller.This will make our code easier.
+
+    int temp=0;
+    for(int k=0; k<ArraySize-1; k++)
+        for(int j=0; j<ArraySize-1; j++)
+        {
+            if(arr[j]<arr[j+1])
+            {
+                temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    //step 4. Filling the A array
+    vector<int> A = arr;
+    int totalAmountOfA=0;
+    int m=0;
+
+    while(totalAmountOfA < valueToReach)
+    {
+        A[m] = arr[m];
+        totalAmountOfA += A[m];
+        m++;
+
+    }
+
+    cout<<"Total amount:"<<totalAmount<<endl;
+    cout<<"valueToReach:"<<valueToReach<<endl;
+    cout<<"Total amount of A:"<<totalAmountOfA<<endl;
+    return A;
+}
+
+
+
+
+
+int main()
+{
+    vector <int> arr= {5,8,1,4,3,7};
+
+
+    vector <int> result = minimalHeaviestSetA(arr);
+    for(int i = 0 ; i< result.size(); i++)
+    {
+        cout<<"Anin "<< i << ". elemani"<< result[i] <<endl;
+    }
+    cout<< result.size();
+    return 0;
+}
